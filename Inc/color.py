53a75e858efa871d8658bb37cd3aa3d2ba5b1616
Felix-Renzikowski <felix.renzi@gmx.de>
2023-12-29 19:53:38 +0100

    Improves image color reduction algorithm considering runtime and quality

--- Inc/color.py
+++ Inc/color.py
@@ -3,26 +3,36 @@ import numpy as np
 
 class Color:
 
-    def __init__(self, color_rgb, percentage, bgr=False):
+    @classmethod
+    def create_color_from_lab_and_percentage (cls, lab, percentage):
+        color_rgb = np.array ([])
+
+
+    def __init__(self, color_rgb, percentage, own_color_key, count_pixel, bgr=False):
         if bgr:
             self.rgb = np.array([color_rgb[2], color_rgb[1], color_rgb[0]])
+            self.bgr = np.array([color_rgb[0], color_rgb[1], color_rgb[2]])
         else:
             self.rgb = np.array([color_rgb[0], color_rgb[1], color_rgb[2]])
+            self.bgr = np.array([color_rgb[2], color_rgb[1], color_rgb[0]])
         self.lab = None
         self.percentage = percentage
-        self.distance = []
+        self.count_pixel = count_pixel
+        self.distance = {}
         self.total_distance = 0
+        self.this_color_key = own_color_key
+        self.replacement_key = None
 
     def calculate_weighted_distances(self, all_colors):
-        weighted_distances = np.empty((len(all_colors) - 1))
 
-        for k in range(0, len(all_colors)):
-            if all_colors[k] != self:
-                self.distance.append(
-                    [all_colors[k], np.linalg.norm(self.lab - all_colors[k].lab) * all_colors[k].percentage,
-                     np.linalg.norm(self.lab - all_colors[k].lab)])
-        for i in self.distance:
-            self.total_distance += i[1]
+        for key in all_colors.keys():
+            if all_colors[key] != self and key not in self.distance:
+                distance_value = [np.linalg.norm(self.lab - all_colors[key].lab) / all_colors[key].percentage,
+                                  np.linalg.norm(self.lab - all_colors[key].lab)]
+                self.distance[key] = distance_value
+                all_colors[key].distance[self.this_color_key] = distance_value
+        for value in self.distance.values():
+            self.total_distance += value[0]
 
     def calculate_lab(self):
         def f(t):
@@ -50,8 +60,12 @@ class Color:
                              500 * (f(color_xyz[0]) - f(color_xyz[1])),
                              200 * (f(color_xyz[1]) - f(color_xyz[2]))])
 
-    def get_distance_to_color(self, color):
-        for distance_array in self.distance:
-            if np.array_equal(distance_array[0].rgb, color.rgb):
-                return distance_array[2]
-        return 0
+    def get_distance_to_color(self, color_key):
+        if color_key == self.this_color_key:
+            return 0
+        else:
+            return self.distance[color_key][1]
+
+    def get_lab_distance_multiplied_with_percentage(self, lab):
+        return np.linalg.norm(self.lab - lab) * self.percentage
+
--- main.py
+++ main.py
@@ -2,51 +2,116 @@ import cv2
 import numpy as np
 from datetime import datetime
 from Inc.color import Color
+from PIL import Image
 
-KEEP_COLORS = 4
+KEEP_COLORS = 24
+COUNT_THREADS = 1
+PATH_TO_IMAGE = "img/test2"
+IMAGE_EXTENSION = ".jpg"
 
 
 def get_all_unique_colors_sorted(image):
     colors, count = np.unique(image.reshape(-1, image.shape[-1]), axis=0, return_counts=True)
     total_pixels = image.shape[0] * image.shape[1]
-    sorted_colors = []
-    for i in range(0, len(colors)):
-        sorted_colors.append(Color(colors[i], count[i]/total_pixels, bgr=True))
+    sorted_colors = {}
+    for k in range(0, len(colors)):
+        sorted_colors[str(colors[k])] = Color(colors[k], count[k] / total_pixels, str(colors[k]), count[k], bgr=True)
     return sorted_colors
+
+
+def append_common_colors_rest(array_to_append, color_dict, count_colors_to_keep):
+    for i in range(1, count_colors_to_keep):
+        average_lab = get_average_lab(array_to_append)
+        highest_weighted_distance = 0
+        append_color_key = None
+        for color_value in color_dict.values():
+            if color_value not in array_to_append:
+                dist = color_value.get_lab_distance_multiplied_with_percentage(average_lab)
+                if dist > highest_weighted_distance:
+                    highest_weighted_distance = dist
+                    append_color_key = color_value.this_color_key
+        array_to_append.append(color_dict[append_color_key])
+
+
+def get_average_lab(array_to_calculate):
+    labs = []
+    for i in array_to_calculate:
+        labs.append(i.lab*i.percentage)
+    return np.mean(np.array(labs))
+
+
+def replace_colors_with_common_colors(image, color_dict):
+    last_print_percent = -1
+    for i in range(0, len(image)):
+        if int(i / len(image) * 100) >= last_print_percent:
+            last_print_percent += 1
+            print(f"{last_print_percent} %")
+        for k in range(0, len(image[i])):
+            image[i][k] = np.array(all_colors[color_dict[str(image[i][k])].replacement_key].bgr)
+    return image
 
 
 start_time = datetime.now()
 print("Started Program...")
-print(str(datetime.now()-start_time) + ": Loading Image...")
-img = cv2.imread('Malen_Nach_Zahlen_Test.jpg')
-print(str(datetime.now()-start_time) + ": Getting and sorting all Colors...")
-all_colors = get_all_unique_colors_sorted(img)
-print(str(datetime.now()-start_time) + ": Converting Colors to Lab color range...")
-for color in all_colors:
+print(str(datetime.now() - start_time) + ": Loading Image...")
+
+pil_img = Image.open(PATH_TO_IMAGE + IMAGE_EXTENSION)
+pil_img = pil_img.convert("P", palette=Image.ADAPTIVE, colors=256)
+pil_img.save(PATH_TO_IMAGE + ".png")
+img_bgr = cv2.imread(PATH_TO_IMAGE + ".png")
+print(str(datetime.now() - start_time) + ": Getting and sorting all Colors...")
+all_colors = get_all_unique_colors_sorted(img_bgr)
+print("TOTAL COLORS: " + str(len(all_colors)))
+print(str(datetime.now() - start_time) + ": Converting Colors to Lab color range...")
+for color in all_colors.values():
     color.calculate_lab()
-print(str(datetime.now()-start_time) + ": Getting weighted distances between colors...")
-for color in all_colors:
-    color.calculate_weighted_distances(all_colors)
-for i in all_colors:
-    print(i.total_distance)
+print(str(datetime.now() - start_time) + ": Getting weighted distances between colors...")
+last_print = -1
+for index, key in enumerate(all_colors):
+    if int(index / len(all_colors) * 100) >= last_print:
+        last_print += 1
+        print(f"{last_print} %")
+    all_colors[key].calculate_weighted_distances(all_colors)
 print(str(datetime.now() - start_time) + ": Sorting colors by total weighted distance...")
-sorted_all_colors = sorted(all_colors, key=lambda color: color.total_distance)
-common_colors = sorted_all_colors[:KEEP_COLORS]
-common_colors_with_rel_colors = []
-print(str(datetime.now()-start_time) + ": Group nearest Colors to Common Colors...")
-for color in common_colors:
-    common_colors_with_rel_colors.append([color, []])
-for color in sorted_all_colors:
-    smallest_distance = common_colors[0].get_distance_to_color(color)
-    smallest_distance_common_color = common_colors[0]
-    for common_color in common_colors:
-        if smallest_distance > common_color.get_distance_to_color(color):
-            smallest_distance = common_color.get_distance_to_color(color)
-            smallest_distance_common_color = common_color
-    for common_color_rel_array in common_colors_with_rel_colors:
-        if common_color_rel_array[0] == smallest_distance_common_color:
-            common_color_rel_array[1].append([color, smallest_distance])
-print(common_colors_with_rel_colors[0])
-print(common_colors_with_rel_colors[1])
-print(common_colors_with_rel_colors[2])
-print(common_colors_with_rel_colors[3])
+sorted_all_colors = sorted(all_colors.values(), key=lambda this_color: this_color.total_distance)
+common_colors = [sorted_all_colors[0]]
+append_common_colors_rest(common_colors, all_colors, KEEP_COLORS)
+common_colors_keys = []
+for index in range(0, len(common_colors)):
+    common_colors_keys.append(common_colors[index].this_color_key)
+print(str(datetime.now() - start_time) + ": Group nearest Colors to Common Colors...")
+common_colors_with_rel_colors = {}
+for common_color_key in common_colors_keys:
+    common_colors_with_rel_colors[common_color_key] = []
+for key in all_colors.keys():
+    smallest_distance = all_colors[common_colors_keys[0]].get_distance_to_color(key)
+    smallest_distance_common_color_key = common_colors_keys[0]
+    for common_color_key in common_colors_keys:
+        distance = all_colors[common_color_key].get_distance_to_color(key)
+        if smallest_distance > distance:
+            smallest_distance = distance
+            smallest_distance_common_color_key = common_color_key
+    common_colors_with_rel_colors[smallest_distance_common_color_key].append(key)
+print(str(datetime.now() - start_time) + ": Calculating replacement colors for common Color groups...")
+for common_color_key in common_colors_with_rel_colors:
+    labs = []
+    pixels = []
+    for color_key in common_colors_with_rel_colors[common_color_key]:
+        labs.append(all_colors[color_key].lab)
+        pixels.append(all_colors[color_key].count_pixel)
+    pixels_divided = np.divide(pixels, np.sum(pixels))
+    pixels_divided_reshaped = np.reshape(pixels_divided, (len(pixels_divided), 1))
+    weighted_labs = np.multiply(labs, pixels_divided_reshaped)
+    replacement_color_lab = np.mean(weighted_labs)
+    #TODO: createColorfromLab
+    #TODO: add createdColor as replacement_key to every color in common_color_group
+    #TODO: change code in replacement function to use Color Object instead of key
+print(str(datetime.now() - start_time) + ": Replacing Colors with common colors...")
+img_bgr = replace_colors_with_common_colors(img_bgr, all_colors)
+print("TOTAL COLORS NOW: " + str(len(get_all_unique_colors_sorted(img_bgr))))
+print(str(datetime.now() - start_time) + ": Saving image...")
+cv2.imwrite('output2.png', img_bgr)
+# print(common_colors_with_rel_colors[0])
+# print(common_colors_with_rel_colors[1])
+# print(common_colors_with_rel_colors[2])
+# print(common_colors_with_rel_colors[3])
